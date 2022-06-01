d9b0245d9fc7230deb27a7979d4629f70ff67720
Filipe Dircksen <filipeduf@gmail.com>
2022-06-01 10:01:11 -0300

    feat: new middleware and connection class

--- server/app/source/app.ts
+++ server/app/source/app.ts
@@ -3,6 +3,7 @@ import { Server as ServerIo } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
 import AuthService from './services/auth_service';
+import { Connection } from './managers/connection';
 
 
 const app = express();
@@ -17,20 +18,26 @@ const io = new ServerIo(server, {
 });
 
 io.use((socket, next) => {
-  // let tokenObject = JSON.parse(socket.handshake.query.token)
-  // console.log(authService.authUser(socket.handshake.query.token));
-  // if(!authService.authUser(data.token)){
-  //   socket.emit('authentication failed', { message: 'Disconnected', session: socket.id }); 
-  //   socket.disconnect();
-  // }
-  // socket.emit('authenticated', { message: 'Ready to chat', session: socket.id }); 
-  console.log(socket.handshake.query.token)
-  return socket.disconnect();
+  let tokenObject:string = '';
+  if ((typeof socket.handshake.query.token) === 'string' )
+      tokenObject = <string>socket.handshake.query.token;
+  else{
+    socket.emit('authentication failed', { message: 'Disconnected', session: socket.id });
+    socket.disconnect();
+  }
+  if (!authService.authUser(tokenObject)) {
+    socket.emit('authentication failed', { message: 'Disconnected', session: socket.id });
+    socket.disconnect();
+  }else{
+    socket.emit('authenticated', { message: 'Ready to chat', session: socket.id });
+    console.log(socket.handshake.query.token)
+  }
+  return next();
 });
 
 io.on('connection', socket => {
-  
-  socket.emit('on connected', { message: 'connected', session: socket.id }); 
+
+  socket.emit('on connected', { message: 'connected', session: socket.id });
   console.log(socket.id);
 
   socket.on('on loaded', data => {
@@ -43,7 +50,7 @@ io.on('connection', socket => {
       user: chat.session.id,
       message: chat.message,
       timesmap: chat.timestamp,
-      room : chat.room
+      room: chat.room
     });
     // io.in("room1").socketsLeave("room1");
   });
---
+++ server/app/source/managers/connection.ts
@@ -0,0 +1,41 @@
+export class Connection {
+    private _course_class_id: string;
+    private _course_id: string;
+    private _socket_id: string;
+    private _user_id: string;
+    
+    
+    constructor (courseClassId, socketId, courseId, userId) {
+        this._course_class_id = courseClassId;
+        this._socket_id = socketId;
+        this._course_id = courseId;
+        this._user_id = userId;
+    }
+    
+    
+    public get course_id(): string {
+        return this._course_id;
+    }
+    public set course_id(value: string) {
+        this._course_id = value;
+    }
+    public get user_id(): string {
+        return this._user_id;
+    }
+    public set user_id(value: string) {
+        this._user_id = value;
+    }
+    public get socket_id(): string {
+        return this._socket_id;
+    }
+    public set socket_id(value: string) {
+        this._socket_id = value;
+    }
+    public get course_class_id(): string {
+        return this._course_class_id;
+    }
+    public set course_class_id(value: string) {
+        this._course_class_id = value;
+    }
+
+}
